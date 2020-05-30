d83c8f192f0dd4557ad8819c4e093648147685fc
TotoAfreeca <Tomasz.Burblis@gmail.com>
2020-05-30 18:55:28 +0200

    fixed long notation in reports

--- test_mnist.py
+++ test_mnist.py
@@ -1,9 +1,26 @@
 import numpy as np
-
+from keras.datasets import mnist
 from neural_network import NeuralNetwork
 from HiddenLayer import HiddenLayer
-from keras.datasets import mnist
 from keras.utils import np_utils
+
+
+
+def sigmoid_unipolar_function(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_unipolar_prime(z):
+    return sigmoid_unipolar_function(z) * (1 - sigmoid_unipolar_function(z))
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_prime(x):
+    return 1-np.tanh(x)**2
+
+np.set_printoptions(suppress=True)
 
 # load MNIST from server
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -25,13 +42,13 @@ y_test = np_utils.to_categorical(y_test)
 
 # Network
 net = NeuralNetwork()
-net.add(HiddenLayer(28*28, 100))                # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
-net.add(HiddenLayer(100, 50))                   # input_shape=(1, 100)      ;   output_shape=(1, 50)
-net.add(HiddenLayer(50, 10))                    # input_shape=(1, 50)       ;   output_shape=(1, 10)
+net.add_layer(HiddenLayer(28*28, 100, sigmoid_unipolar_function, sigmoid_unipolar_prime ))                # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
+net.add_layer(HiddenLayer(100, 50, sigmoid_unipolar_function, sigmoid_unipolar_prime))                   # input_shape=(1, 100)      ;   output_shape=(1, 50)
+net.add_layer(HiddenLayer(50, 10, sigmoid_unipolar_function, sigmoid_unipolar_prime))                    # input_shape=(1, 50)       ;   output_shape=(1, 10)
 
 # train on 1000 samples
 # as we didn't implemented mini-batch GD, training will be pretty slow if we update at each iteration on 60000 samples...
-net.fit(x_train[0:1000], y_train[0:1000], epochs=35, learning_rate=0.1)
+net.train(x_train[0:1000], y_train[0:1000], epochs=35, learning_rate=0.1)
 
 # test on 3 samples
 out = net.predict(x_test[0:3])
